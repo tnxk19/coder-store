ea1db1820b900846663ac376a6c1c7af569cdf88
Tran Nguyen Xuan Khanh <trannguyenxuankhanh15@gmail.com>
2026-07-01 23:43:00 +0700

    done

--- src/components/ProductSort.js
+++ src/components/ProductSort.js
@@ -1,19 +1,19 @@
 import React from "react";
+import { MenuItem } from "@mui/material";
 import { FSelect } from "./form";
 
 function ProductSort() {
   return (
-    <FSelect name="sortBy" label="Sort By" size="small" sx={{ width: 300 }}>
-      {[
-        { value: "featured", label: "Featured" },
-        { value: "newest", label: "Newest" },
-        { value: "priceDesc", label: "Price: High-Low" },
-        { value: "priceAsc", label: "Price: Low-High" },
-      ].map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.label}
-        </option>
-      ))}
+    <FSelect
+      name="sortBy"
+      label="Sort By"
+      size="small"
+      sx={{ width: 300 }}
+    >
+      <MenuItem value="featured">Featured</MenuItem>
+      <MenuItem value="newest">Newest</MenuItem>
+      <MenuItem value="priceDesc">Price: High-Low</MenuItem>
+      <MenuItem value="priceAsc">Price: Low-High</MenuItem>
     </FSelect>
   );
 }
